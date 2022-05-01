48d4bdc2899b12c4fafb6efe3583b39925dbc8ee
loco9939 <kls9939@naver.com>
2022-05-02 00:21:40 +0900

    링크 스타일 바꾸기

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 /* eslint-disable */
 import "./App.css";
 import { Button, Navbar, Container, Nav, Col, Row } from "react-bootstrap";
-import shoes1 from "./img/shoes1.jpeg";
-import shoes2 from "./img/shoes2.jpeg";
-import shoes3 from "./img/shoes3.jpeg";
 import { useState } from "react";
 import data from "./data";
+import { Routes, Route, Link } from "react-router-dom";
 
 function App() {
   let [shoes] = useState(data);
@@ -16,33 +14,66 @@ function App() {
         <Container>
           <Navbar.Brand href="#home">I_Shopping</Navbar.Brand>
           <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#features">Features</Nav.Link>
-            <Nav.Link href="#pricing">Pricing</Nav.Link>
+            <Link to="/">홈</Link>
+            <Link to="/detail">상세페이지</Link>
           </Nav>
         </Container>
       </Navbar>
 
-      <div className="main-bg"></div>
-      <div className="container">
-        <div className="row">
-          {shoes.map((a, i) => {
-            return <Card shoes={a} i={i} key={i} />;
-          })}
-        </div>
-      </div>
+      <Routes>
+        <Route exact path="/" element={<Home 신발={shoes} />} />
+        <Route path="/detail" element={<Detail 신발={shoes} />} />
+      </Routes>
     </div>
   );
 }
 
 function Card(props) {
-  const { shoes, i } = props
   return (
     <>
       <div className="col-md-4">
-        <img src={'shoes'+(i+1)} width="100%" />
-        <h4>{shoes.title}</h4>
-        <p>{shoes.content}</p>
+        <img src={"./img/shoes" + (props.i + 1) + ".jpeg"} width="100%" />
+        <h4>{props.신발.title}</h4>
+        <p>{props.신발.content}</p>
+      </div>
+    </>
+  );
+}
+
+function Home(props) {
+  return (
+    <>
+      <div className="main-bg"></div>
+      <div className="container">
+        <div className="row">
+          {props.신발.map((a, i) => {
+            return <Card 신발={a} i={i} key={i} />;
+          })}
+        </div>
+      </div>
+    </>
+  );
+}
+
+function Detail(props) {
+  console.log(props.신발[0].id);
+  return (
+    <>
+      <div className="container">
+        <div className="row">
+          <div className="col-md-6">
+            <img
+              src={"./img/shoes" + (props.신발[0].id + 1) + ".jpeg"}
+              width="100%"
+            />
+          </div>
+          <div className="col-md-6">
+            <h4 className="pt-5">상품명</h4>
+            <p>상품설명</p>
+            <p>120000원</p>
+            <button className="btn btn-danger">주문하기</button>
+          </div>
+        </div>
       </div>
     </>
   );
